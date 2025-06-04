3788a0baa3c9e25e1b8532e64f3b36e59f50e9f5
ONOCHAO <kirilllys1234@gmail.com>
2025-06-04 19:52:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import Mystat
+
+
+user1 = Mystat("lysen_km00", "Zef7r2*y")  
+print(user1.get_auth()) 
+print(user1.get_marks())  
+print(user1.get_schedule())
+print(user1.get_schedule())
+
+print(user1.calculate_average())  
+
